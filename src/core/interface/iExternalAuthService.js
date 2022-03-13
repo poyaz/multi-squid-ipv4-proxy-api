6acfe9d8076b27c2ba132fe848a24690ce5f3269
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-03-13 10:33:27 +0330

    Add interface for external auth
    Add model for external auth

---
+++ src/core/interface/iExternalAuthService.js
@@ -0,0 +1,41 @@
+/**
+ * Created by pooya on 3/13/22.
+ */
+
+class IExternalAuthService {
+  /**
+   *
+   * @param {string} platform
+   * @return {Promise<(Error|OauthModel)[]>}
+   */
+  async getOptions(platform) {
+    const error = new Error('The method has to be overridden by subclasses.');
+    error['args'] = { platform };
+    throw error;
+  }
+
+  /**
+   *
+   * @param {string} platform
+   * @return {Promise<(Error|string)[]>}
+   */
+  async auth(platform) {
+    const error = new Error('The method has to be overridden by subclasses.');
+    error['args'] = { platform };
+    throw error;
+  }
+
+  /**
+   *
+   * @param {string} platform
+   * @param {string} code
+   * @return {Promise<(Error|UserModel)[]>}
+   */
+  async verify(platform, code) {
+    const error = new Error('The method has to be overridden by subclasses.');
+    error['args'] = { platform, code };
+    throw error;
+  }
+}
+
+module.exports = IExternalAuthService;
---
+++ src/core/model/oauthModel.js
@@ -0,0 +1,17 @@
+/**
+ * Created by pooya on 3/13/22.
+ */
+
+/**
+ *
+ * @property {string} id
+ * @property {string} platform
+ * @property {string} redirectUrl
+ */
+class OauthModel {
+  id = undefined;
+  platform = undefined;
+  redirectUrl = undefined;
+}
+
+module.exports = OauthModel;
