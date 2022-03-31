42def81d370921a7c59f71338ff7e94c05655cc4
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-03-31 22:13:08 +0000

    Fix `replaceAll` function error

--- src/api/http/oauth/controller/oauthController.js
+++ src/api/http/oauth/controller/oauthController.js
@@ -68,7 +68,7 @@ class OauthController {
     const loginExternalUserOutputModel = new LoginExternalUserOutputModel(this.#jwt);
     const result = loginExternalUserOutputModel.getOutput(data);
 
-    const redirectUrl = this.#oauthHtmlPage.address.replaceAll(this.#oauthHtmlPage.key, result.token);
+    const redirectUrl = this.#oauthHtmlPage.address.split(this.#oauthHtmlPage.key).join(result.token);
 
     return [null, redirectUrl];
   }
