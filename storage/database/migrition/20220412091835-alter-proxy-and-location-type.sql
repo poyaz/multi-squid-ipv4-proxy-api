1974610eb023b9f1b439b3627696a119b7511c23
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-04-13 09:21:46 +0430

    Refactor sql

--- storage/database/migrition/20220412091835-alter-proxy-and-location-type.sql
+++ storage/database/migrition/20220412091835-alter-proxy-and-location-type.sql
@@ -1,3 +1,5 @@
-alter table bind_address add column proxy_type varchar(10) default '-';
+alter table bind_address
+    add column proxy_type varchar(10) default '-';
 
-alter table bind_address add column country_code varchar(10) default '-';
+alter table bind_address
+    add column country_code varchar(10) default '-';
